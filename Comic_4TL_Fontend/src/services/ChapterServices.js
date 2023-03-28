0f0932409338adbfa55c1eaba5a5132a82d0c720
HieuSon11 <90641726+HieuSon11@users.noreply.github.com>
2023-03-28 07:58:18 +0700

    commit
    update chapter

--- Comic_4TL_Fontend/src/services/ChapterServices.js
+++ Comic_4TL_Fontend/src/services/ChapterServices.js
@@ -3,6 +3,6 @@ import api from "./api";
 class ChapterServices{
     // Get all chapters by manga id
     getChapters(Id){
-        return api.get(`/chapters/${Id}`);
+        return api.get("/chapter/${Id}");
     }
 }
